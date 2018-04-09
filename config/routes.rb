a0cc63dcf4ef38b1dd017c4eb5e27a0b2fd1f4e3
Boyuan Song <sby318@gmail.com>
2018-04-10 04:24:41 +0800

    create vote v1.0

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   get "bmi", to: "bmi#index"
   post "bmi/result", to: "bmi#result"
 
+  resources :candidates do
+    member do
+      post :vote
+    end
+  end
+
   resources :posts
   resources :users
 
