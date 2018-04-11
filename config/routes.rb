510630c0f01da71030c0d2ecef149515b7841e73
Boyuan Song <sby318@gmail.com>
2018-04-12 06:59:59 +0800

    create root_page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get "bmi", to: "bmi#index"
   post "bmi/result", to: "bmi#result"
 
+  root "welcome#index"
+
   resources :candidates do
     member do
       post :vote
