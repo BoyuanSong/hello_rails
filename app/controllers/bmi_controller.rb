904aebd771d2ff2cd360cbae2e79ca17ef8b7138
Boyuan Song <sby318@gmail.com>
2018-04-12 05:00:59 +0800

    complete bmi

--- app/controllers/bmi_controller.rb
+++ app/controllers/bmi_controller.rb
@@ -8,7 +8,18 @@ class BmiController < ApplicationController
 
     if @bmi_calculator.valid?
       @bmi = @bmi_calculator.perform
+
+      if @bmi >= 0 && @bmi <= 18
+        @comment = "體重過輕了"
+      elsif @bmi > 18 && @bmi <= 25
+        @comment = "體重適中"
+      elsif @bmi > 25
+        @comment = "體重過重請注意身體健康"
+      else
+        @comment = "請輸入正確的身高體重"
+      end
     else
+      @msg = "請輸入身高體重"
       render :index
     end
   end
