8c5c08a32b3f2416779800ee8a16a1e893f6a897
Boyuan Song <sby318@gmail.com>
2018-05-25 16:01:27 +0800

    fix cart part

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,13 @@
 class CartsController < ApplicationController
   def add
-    current_cart.add_item(params[:id])
-    session[:cart9487] = current_cart.serialize
+    @cart.add_item(params[:id])
+    session[:cart318] = @cart.serialize
 
     redirect_to products_path, notice: "已加入購物車"
   end
 
   def destroy
-    session[:cart9487] = nil
+    session[:cart318] = nil
     redirect_to products_path, notice: "購物車已清空"
   end
 end
